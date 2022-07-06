329449c9a7613378377c13e4ecad094a951c50cb
CrackHegg <107590615+CrackHegg@users.noreply.github.com>
2022-07-06 17:08:34 -0500

    Update atari_emulator.py

--- atari_emulator.py
+++ atari_emulator.py
@@ -1,5 +1,5 @@
 import numpy as np
-from ale_python_interface import ALEInterface
+from ale-py import ALEInterface
 from scipy.misc import imresize
 import random
 from environment import BaseEnvironment, FramePool,ObservationPool
